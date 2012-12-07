590dfb4e882a260398abf0f91f45c7fab1ff7099
mohamadmahtabi <mohammadmahtabi@gmail.com>
2012-12-07 20:24:34 +0330

    sketching added!

--- Implementation/Chapter 2/fixed_point_iterator.m
+++ Implementation/Chapter 2/fixed_point_iterator.m
@@ -1,12 +1,21 @@
-function root = fixed_point_iterator( gfunc, start, max_step, tol )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-xis = zeros(max_step,1);
+function root = fixed_point_iterator( gfunc, start, tol )
+%fixed point iterator This funtion assumes that we have a g(x) function
+%that g(x)=x. 
+%
+% OUTPUT:
+%   returns NaN if the method can't converge.
+err = zeros(100,1);
+xis = zeros(100,1);
 xis(1,1) = start;
-errs
+
 for i=2:max_step+1
     xis(i,1) = feval(gfunc,xis(i-1));
-    if(abs(xis(i,1)-xis(i-1,1))<tol)
+    err(i,1) = abs(xis(i,1)-xis(i-1,1));
+    if(i>2 && err(i,1)>err(i-1,1))
+        root = NaN;
+        return;
+    end
+    if(err(i,1)<tol)
         root = xis(i,1);
         return;
     end
